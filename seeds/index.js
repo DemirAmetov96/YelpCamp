9fba286ee526cc151dbadc61ea3ddb98a0ef11e2
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-05-12 10:42:56 +0100

    Adding Images
    [+] Added image property to campgrounds schema
    [*] In schema changed price from String to Number
    [+] Added unsplash image generator link to image property in seeding function
    [+] Added a lorem ipsum description to seeding function
    [+] Added image and description HTML tags to show.ejs

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'A few clumsy towers in the Ginger Bread Man briskly ate a couple dazzling princes. The fast princess rather goes a couple wonderful Dumbos. Dumbos of the horses happily showed a Cinderella. Those Rapunzels wishfully eat a witty Rapunzel. A beautiful Fairy God Mother never saw a curse. A Snow White happily sees princesses. Some fast fairies really cook those delightful towers.',
+            price: price
         })
         await camp.save();
     }
