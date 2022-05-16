8fb193e0d243e4fbec6dfe445d622b214f4f2cdc
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-05-16 14:08:13 +0100

    JOI Schema Validation
    [+] NPM installed joi
    [+] Added joi schema to validate post input for new campground

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const Campground = require('./models/campground');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -39,7 +42,18 @@ app.get('/campgrounds/new', (req, res) =>
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+        }).required()
+    });
+    const result = campgroundSchema.validate(req.body);
+    if(result.error)
+    {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
