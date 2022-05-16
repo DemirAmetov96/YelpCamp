50d2d8311d1a045413110337d991ab0e665a426e
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-05-16 11:56:56 +0100

    Defining Error Template
    [+] Added error.ejs HTML template file for error pages
    [*] Changed middleware error function to render error.ejs and pass error object

--- app.js
+++ app.js
@@ -72,8 +72,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong!' } = err;
-    res.status(statusCode).render(message)
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
