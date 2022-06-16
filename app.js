27dffd3559b4f5cfb68b16bdcbfaf6a0068267b3
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-06-16 18:32:51 +0200

    Breaking Out Review Routes
    [+] Create a routes.js file in the routes directory
    [*] Move all of the routes and middleware for the reviews to the new review.js file
    [+] Import the reviews.js router in our main "app.js" and use it and set the route suffix for all review routes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -30,35 +31,8 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
 app.use('/campgrounds', campgrounds)
-
-app.post('/', validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review')
-    res.redirect(`/campgrounds/${id}`);
-}))
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('home')
