f726a2c8dfc85bc0a8edddc7e21c77df8c2d35d3
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-06-30 23:36:57 +0200

    Register Form
    [+] Created a users.js routes file in the routes directory
    [+] Added a GET and POST routes inside the users.js file for the user registration functionality
    [+] Imported the routes into the main "app.js" file and set it to be used on every request
    [+] Created a register.ejs page in the views/users directory to be served on the new user GET route

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
-    email: {
+    email:
+    {
         type: String,
         required: true,
         unique: true
