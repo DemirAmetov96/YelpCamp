d7c65e16ca2f0415e1f5eb2da056c233d4f8293f
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-07-15 23:51:43 +0100

    Centering The Map On A Campground
    [+] Imported the campground object into our EJS file to be used in our ShowMap.js script
    [+] Using the campground object we access the coordinates and center the map around there as well as add a marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-    center: [0,0], // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
