f41985840c3ca3ac6e8db509e2b67ae28802ffb8
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-07-18 18:27:28 +0100

    Setting Up Mongo Atlas
    [*] Changed the connection to our cloud based online mongo database instead of our local one

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl,
+{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
