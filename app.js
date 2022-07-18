9e8270e9250f91a93fd28b350e079ade59ce0454
Demir Ametov <44241106+DemirAmetov96@users.noreply.github.com>
2022-07-18 14:40:16 +0100

    Using Helmet
    [+] Added helmet middleware for HTTP header security

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -56,6 +57,7 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({contentSecurityPolicy: false}));
 
 app.use(passport.initialize());
 app.use(passport.session());
